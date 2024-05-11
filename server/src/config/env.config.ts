b572a10db85b9e4a60558c88025c2010eab39e20
SEANGLAY <seanglyseanglay@gmail.com>
2024-05-11 10:28:00 +0700

    update env file

--- server/src/config/env.config.ts
+++ server/src/config/env.config.ts
@@ -3,7 +3,6 @@ import { getOsEnv } from "../utils/get.env";
 export const Env = {
     NODE_ENV: getOsEnv("NODE_ENV"),
     APP_PORT: getOsEnv("APP_PORT"),
-    ALLOWED_ORIGINS: getOsEnv("ALLOWED_ORIGINS"),
     ALLOWED_HTTP_LOG: parseInt(getOsEnv("ALLOWED_HTTP_LOG")),
     MONGODB_URI: getOsEnv("MONGODB_URI"),
 };
